c2b1eda75e8c4c87785e81bd9ba3fe25aed30cb4
XY <1106693417@qq.com>
2023-04-22 16:06:36 +0800

    更新了使用TD算法训练21点游戏环境的内容，但是效果并不好。符合之前我对训练效果的猜想...

---
+++ BlackJack/BlackJackTest.py
@@ -0,0 +1,12 @@
+from BlackJackEnv import *
+from BlackJackAgent import *
+
+env = BlackJackEnvironment()
+agent = BlackJackAgent(env)
+rounds = 10000
+
+agent.Q_learning_algorithm(rounds=rounds, epsilon=0.9)
+print("Training complete")
+info = agent.play_with_dealer()
+print("Player win rate: {:.2f}%   Dealer win rate:{:.2f}%   Not lose rate:{:.2f}%"
+      .format(info[0] / rounds * 100, info[1] / rounds * 100, (rounds - info[1]) / rounds * 100))
